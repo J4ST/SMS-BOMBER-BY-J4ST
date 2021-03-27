b1757cae901b396fe442c1e2e2581b6d6120e463
J4ST <81233481+J4ST@users.noreply.github.com>
2021-03-27 19:47:36 +0300

    Add files via upload

---
+++ Termux.sh
@@ -0,0 +1,8 @@
+echo "Gerekli moduller yukleniyor dc.J#8781"
+pkg install python
+pkg install python3
+pkg install python3-pip
+pip install requests
+pip install colorama
+echo "Moduller yuklendi."
+python3 bomber.py
